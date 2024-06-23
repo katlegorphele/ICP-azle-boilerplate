09c16828951426343e376ce02bb6a5e0b83d6b25
katlegorphele <katlegorphele@gmail.com>
2024-06-23 19:03:10 +0200

    Adding cre and fetching messages endpoints

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,39 @@ export default Server(() => {
     const app = express();
     app.use(express.json);
 
+    // ENDPOINTS
+
+    // Creating a new message
+    app.post('/messages', (req, res) => {
+        const message: Message = {id: uuidv4(), createdAt: getCurrentDate(), ...req.body};
+        messageStorage.insert(message.id, message);
+        res.json(message);
+    });
+
+    // Get all messages
+    app.get('/messages', (rep, res) => {
+        res.json(messageStorage.values())
+    });
+
+    // Get message by id
+    app.get('/messages/:id', (req, res) => {
+        const messageId = req.params.id;
+        /**
+         * The get method returns an Opt type, which
+         * represents the possibility of a value being
+         * present (Some) or absent (None).
+         */
+        const messageOpt = messageStorage.get(messageId);
+        if ("None" in messageOpt) {
+            res.status(404).send(`the messages with id ${messageId} not found`);
+        } else {
+            res.json(messageOpt.Some);
+        }
+    });
+
+    // Updating a message
+    
+
     return app.listen();
 });
 
